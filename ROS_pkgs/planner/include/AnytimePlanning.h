8b8159467d7383ca7726156c9405742223386f9a
CVEarp <cavie17@student.sdu.dk>
2018-04-24 16:21:42 +0200

    Started shared library for anytime planning

---
+++ ROS_pkgs/planner/include/AnytimePlanning.h
@@ -0,0 +1,70 @@
+// Anytime Dynamic Path Planning Shared Library
+
+// Implemented fr Robotics & Computer Vision 2 course 2018, SDU Robotics
+// Carlos, Sergi, Richárd & Mathesh
+// Uses RobWork & CAROS
+
+// AnytimePlanning.h
+
+#include <iostream>
+#include <fstream> 
+#include <rw/rw.hpp>
+#include <rwlibs/pathplanners/rrt/RRTPlanner.hpp>
+#include <rwlibs/pathplanners/rrt/RRTQToQPlanner.hpp>
+#include <rwlibs/proximitystrategies/ProximityStrategyFactory.hpp>
+#include <vector>
+#include <math.h> 
+#include <numeric>
+#include <algorithm>
+#include <iterator>
+#include <boost/foreach.hpp>
+#include <string>
+#include <vector>
+#include <rw/trajectory/CubicSplineFactory.hpp>
+
+using namespace std;
+using namespace rw::common;
+using namespace rw::math;
+using namespace rw::kinematics;
+using namespace rw::loaders;
+using namespace rw::models;
+using namespace rw::pathplanning;
+using namespace rw::proximity;
+using namespace rw::trajectory;
+using namespace rwlibs::pathplanners;
+using namespace rwlibs::proximitystrategies;
+
+
+class AnytimePlanning{
+  public: 
+	
+	void Load_WorkCell(const string wc_name, const string dev_name);
+	bool checkCollisions(const State &state, const CollisionDetector &detector, const Q &q);
+	QPath get_path(double epsilon, State state, rw::math::Q from, rw::math::Q to);
+	QPath get_trajectory(QPath path, rw::math::Q dq_start, rw::math::Q dq_end);
+
+  private: 
+	/*const string wc_name;
+	const string dev_name;*/
+	rw::models::WorkCell::Ptr wc;
+	Device::Ptr device;
+	bool dev_found;
+	bool wc_found;
+	rw::pathplanning::QToQPlanner::Ptr planner;
+
+
+
+}; // AnytimePlanning
+
+
+
+/*
+
+	Functions needed:
+
+		1. send_path()	------> ROS 
+		2. find_obstacles() (get_ball_position) ------> ROS
+		3. update_obstacle_map()  -------> ROS
+		4. invalidate_nodes()
+
+*/
---
+++ ROS_pkgs/planner/src/AnytimePlanning.cpp
@@ -0,0 +1,184 @@
+// Anytime Dynamic Path Planning Shared Library
+
+// Implemented fr Robotics & Computer Vision 2 course 2018
+// SDU Robotics
+// Carlos, Sergi, Richárd & Mathesh
+
+#include <AnytimePlanning.h>
+
+
+using namespace std;
+using namespace rw::common;
+using namespace rw::math;
+using namespace rw::kinematics;
+using namespace rw::loaders;
+using namespace rw::models;
+using namespace rw::pathplanning;
+using namespace rw::proximity;
+using namespace rw::trajectory;
+using namespace rwlibs::pathplanners;
+using namespace rwlibs::proximitystrategies;
+
+
+/* NOTE */
+
+// Remember to initialize rw::kinematics::State state variable in the main().
+
+
+void AnytimePlanning::Load_WorkCell(const string wc_name, const string dev_name)
+{
+
+	cout << "WorkCell " << wcFile; //<< " and device " << deviceName << endl;
+
+	// if found, loading workcell
+	wc = WorkCellFactory::load(wc_name);
+	if (wc == NULL) 
+	{
+		cerr << "WorkkCell: " << wc_name << " not found!" << endl;
+		wc_found = false;
+	} // if	
+
+	// and device
+	device = wc->findDevice(dev_name);
+	if (device == NULL) 
+	{
+		cerr << "Device: " << dev_name << " not found!" << endl;
+		dev_found = false;
+	} // if
+
+
+}// Load_WorkCell()
+
+
+/* 
+	In the main, after calling Load_Workcell(), use the following breaking condition:
+
+	if(wc_found == false || dev_found == false)
+		{return 0;}
+*/
+
+
+// Function that looks for collisions at a given state (Q).
+bool AnytimePlanning::checkCollisions(const State &state, const CollisionDetector &detector, const Q &q) 
+{
+	State testState;
+	CollisionDetector::QueryResult data;
+	bool colFrom;
+
+	testState = state;
+	device->setQ(q,testState);
+	colFrom = detector.inCollision(testState,&data);
+	if (colFrom) 
+	{
+		cerr << "Configuration in collision: " << q << endl;
+		cerr << "Colliding frames: " << endl;
+		FramePairSet fps = data.collidingFrames;
+		for (FramePairSet::iterator it = fps.begin(); it != fps.end(); it++) 
+		{
+			cerr << (*it).first->getName() << " " << (*it).second->getName() << endl;
+		} //for
+		return false;
+	} // if
+	return true;
+
+} // checkCollisions()
+
+
+
+// Constraint and Collision strategies; Then Generates path
+QPath AnytimePlanning::get_path(double epsilon, State state, rw::math::Q from, rw::math::Q to)
+{
+	// Set collision detection strategy.
+	CollisionDetector detector(wc, ProximityStrategyFactory::makeDefaultCollisionStrategy()); 
+
+	// Set the planner constraint to build RRT-connect.
+	PlannerConstraint constraint = PlannerConstraint::make(&detector, device, state); 
+	QSampler::Ptr sampler = QSampler::makeConstrained(QSampler::makeUniform(device),constraint.getQConstraintPtr());
+	QMetric::Ptr metric = MetricFactory::makeEuclidean<Q>();
+
+	// Check for collisions at initial configuration.
+	if (!checkCollisions(device, state, detector, from))
+		{return 0;}
+	// Check for collisions at final configuration.
+	if (!checkCollisions(device, state, detector, to))
+		{return 0;}
+
+	/*if(ext1 == true || ext2 == true)
+		{return 0;}*/
+
+	cout << endl;
+	cout << "Initial and Final configurations checked. <=========> Collision Free" << endl;
+	cout << endl;
+
+	planner = RRTPlanner::makeQToQPlanner(constraint, sampler, metric, epsilon, RRTPlanner::RRTConnect);
+
+	QPath path;
+	Timer t;
+	t.resetAndResume();
+	planner->query(from, to, path, MAXTIME);
+	t.pause();
+	cout << endl;
+	cout << "	>> Path's length: " << path.size() << endl;
+	cout << "	>> Computation time: " << t.getTime() << " seconds." << endl;
+	cout << endl;
+
+	// Saving original plan
+	int j = 0;
+	ofstream pf;
+	pf.open("/home/charlie/catkin_ws/src/ROVI2_Object_Avoidance/RWStudio/genfiles/path_original.txt");
+	for (QPath::iterator it = path.begin(); it < path.end(); it++) 
+	{
+		pf << j << ":  " << *it << endl;
+		j++;	
+	} // for
+	cout << "Saved to /home/charlie/catkin_ws/src/ROVI2_Object_Avoidance/RWStudio/genfiles/path_original.txt" << endl;
+	cout << endl;
+	
+	return path;
+
+} // get_path()
+
+
+
+
+// Interpolate path nodes to get a trayectory
+QPath AnytimePlanning::get_trajectory(QPath path, rw::math::Q dq_start, rw::math::Q dq_end)
+{
+
+	int imax = path.size();
+	QPath interpolated_path;
+	vector<double> times;
+	double tt = 0.1;
+	for( int i = 0; i< imax; i++)
+	{
+		times.push_back(tt);
+		tt = tt+0.1;
+	} // for
+	InterpolatorTrajectory<rw::math::Q>::Ptr traj = CubicSplineFactory::makeClampedSpline(path, times, dq_start, dq_end);
+
+	int j = 0;
+	ofstream tf;
+	tf.open("/home/charlie/catkin_ws/src/ROVI2_Object_Avoidance/RWStudio/genfiles/path_interpolated.txt");	
+
+	double t0 = traj->startTime();
+  	double tn = traj->endTime();
+
+	cout << "	>> Trajectory duration: " << tn << " seconds." << endl;
+	
+	for (double t = t0; t <= tn; t += 0.01) 
+	{
+		// Conversion to save to .lua file
+		rw::math::Q q_i(6, traj->x(t)[0], traj->x(t)[1], traj->x(t)[2], traj->x(t)[3], traj->x(t)[4], traj->x(t)[5] );  
+		tf << j << ":  t = " << t << "  |  " << q_i << endl;
+		interpolated_path.push_back(q_i);
+
+	} // for
+	cout << "	>> Trajectory length: " << interpolated_path.size() << " nodes." << endl;
+	cout << "	>> Saved with time steps of 0.01 seconds." << endl;
+	cout << endl;
+	cout << "Saved to /home/charlie/catkin_ws/src/ROVI2_Object_Avoidance/RWStudio/genfiles/path_interpolated.txt" << endl;
+	cout << endl;
+
+	return interpolated_path;
+
+} // get_trajectory()
