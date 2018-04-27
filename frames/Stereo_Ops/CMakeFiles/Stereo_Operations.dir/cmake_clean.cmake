650aae77e895e6a4a12a3fdcd15600f61897677e
Matheshwaranpitchai <matheshwaranpitchai@gmail.com>
2018-04-27 18:07:20 +0200

    Vision part of the project

---
+++ frames/Stereo_Ops/CMakeFiles/Stereo_Operations.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Stereo_Operations.dir/stereo_ops.cpp.o"
+  "Stereo_Operations.pdb"
+  "Stereo_Operations"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Stereo_Operations.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
